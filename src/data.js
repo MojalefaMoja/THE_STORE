eaebaf2da1753b0742611b31ed2d0a4fa2076234
Mojalefa MJAY Moja <87690422+MojalefaMoja@users.noreply.github.com>
2023-09-04 01:50:47 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = {
+    products: [
+        {
+            name: 'Nike slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg', //679 x 829px
+            price: 120,
+            cCountInStock: 10,
+            brand: 'Nike',
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+            name: 'Nike slim pants',
+            slug: 'nike-slim-pant',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 120,
+            cCountInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality pants',
+        },
+        {
+            name: 'Adidas slim shirt',
+            slug: 'Adidas slim-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 120,
+            cCountInStock: 10,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+            name: 'Puma sim pants',
+            slug: 'puma-slim-pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 120,
+            cCountInStock: 10,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+    ],
+}
+
+export default data
